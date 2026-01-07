03068ce07c2594b21c630d7a3ae9cd90d1643edf
Emily-jetlearn <mimidee176@gmail.com>
2026-01-07 19:48:46 +0000

    Update lesson 15 07.01.26.py

--- lesson 15 07.01.26.py
+++ lesson 15 07.01.26.py
@@ -1,13 +1,13 @@
 import cv2
-cap = cv2.VideoCapture("cars video.mp4")
-car_cascade = cv2.CascadeClassifier("cars.xml")
+cap = cv2.VideoCapture(r"C:\Users\olada\OneDrive\Desktop\Opencv\lesson 15\vehicle detection\cars video.mp4")
+car_cascade = cv2.CascadeClassifier(r"C:\Users\olada\OneDrive\Desktop\Opencv\lesson 15\vehicle detection\cars.xml")
 
 while True:
     ret, frames, = cap.read()
     grey = cv2.cvtColor(frames,cv2.COLOR_BGR2GRAY)
     cars = car_cascade.detectMultiScale(grey, 1.1, 1)
     for(x,y,w,h) in cars:
-        cv2.rectangle(frames,(x,y),(x+w, y+h),(0,0,255),2)
+        cv2.rectangle(frames,(x,y),(x+w, y+h),(0,255,0),6)
     cv2.imshow("car detector",frames)
     if cv2.waitKey(33) == 27:
         break
